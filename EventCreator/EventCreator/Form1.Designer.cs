a2087e3efa66c78cd1e73aedc1e6eb4cf3fddc87
tws2xa <tws2xa@virginia.edu>
2014-09-26 23:35:05 -0400

    Test Commit!

--- EventCreator/EventCreator/Form1.Designer.cs
+++ EventCreator/EventCreator/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(288, 292);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(89, 27);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test Button";
+            this.btnTest.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 329);
+            this.Controls.Add(this.btnTest);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnTest;
     }
 }
 
