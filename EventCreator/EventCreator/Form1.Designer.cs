f08f1933f82f1624baee3095df1715b7de12689f
tws2xa <tws2xa@virginia.edu>
2014-10-07 11:16:57 -0400

    Added the tabs.

--- EventCreator/EventCreator/Form1.Designer.cs
+++ EventCreator/EventCreator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EventCreator
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,33 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnTest = new System.Windows.Forms.Button();
+            this.tabMain = new System.Windows.Forms.TabControl();
+            this.tabGeneralInfo = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabMain.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnTest
+            // tabMain
             // 
-            this.btnTest.Location = new System.Drawing.Point(288, 292);
-            this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(89, 27);
-            this.btnTest.TabIndex = 0;
-            this.btnTest.Text = "Test Button";
-            this.btnTest.UseVisualStyleBackColor = true;
+            this.tabMain.Controls.Add(this.tabGeneralInfo);
+            this.tabMain.Controls.Add(this.tabPage2);
+            this.tabMain.Controls.Add(this.tabPage3);
+            this.tabMain.Controls.Add(this.tabPage4);
+            this.tabMain.Controls.Add(this.tabPage5);
+            this.tabMain.Location = new System.Drawing.Point(0, 0);
+            this.tabMain.Name = "tabMain";
+            this.tabMain.SelectedIndex = 0;
+            this.tabMain.Size = new System.Drawing.Size(862, 542);
+            this.tabMain.TabIndex = 0;
             // 
-            // Form1
+            // tabGeneralInfo
+            // 
+            this.tabGeneralInfo.Location = new System.Drawing.Point(4, 22);
+            this.tabGeneralInfo.Name = "tabGeneralInfo";
+            this.tabGeneralInfo.Padding = new System.Windows.Forms.Padding(3);
+            this.tabGeneralInfo.Size = new System.Drawing.Size(854, 516);
+            this.tabGeneralInfo.TabIndex = 0;
+            this.tabGeneralInfo.Text = "General Info";
+            this.tabGeneralInfo.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(854, 516);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(854, 516);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(854, 516);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(854, 516);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(386, 329);
-            this.Controls.Add(this.btnTest);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(861, 538);
+            this.Controls.Add(this.tabMain);
+            this.Name = "frmMain";
+            this.Text = "Event Creator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TabControl tabMain;
+        private System.Windows.Forms.TabPage tabGeneralInfo;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+
     }
 }
 
--- EventCreator/EventCreator/Form1.cs
+++ EventCreator/EventCreator/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace EventCreator
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tabMain.Size = this.Size;
         }
     }
 }
